4a6a2177a80587abc79006132114a20773b459b5
Emanuela <emanuela.velardo1@gmail.com>
2023-02-07 21:57:43 +0100

    ...

--- form_in_php/esercitazione_form/create-user.php
+++ form_in_php/esercitazione_form/create-user.php
@@ -46,55 +46,66 @@ Pagina che riceve i dati
 
                     <div class="mb-3">
                         <label for="first_name" class="form-label">Nome</label>
-                        <input type="text" class="form-control" name="first_name" id="Nome">
+                        <input type="text" class="form-control" name="first_name" id="first_name">
                     </div>
 
                     <div class="mb-3">
                         <label for="last_name" class="form-label">Cognome</label>
-                        <input type="text" class="form-control" name="last_name" id="Cognome">
+                        <input type="text" class="form-control" name="last_name" id="last_name">
                     </div>
 
                     <div class="mb-3">
                         <label for="birthday" class="form-label">Data di nascita</label>
-                        <input type="date" class="form-control" name="birthday" id="data di nascita">
+                        <input type="date" class="form-control" name="birthday" id="birthday">
                     </div>
 
                     <div class="mb-3">
                         <label for="birth_place" class="form-label">Luogo di nascita</label>
-                        <input type="text" class="form-control" name="birth_place" id="Luogo di nascita">
+                        <input type="text" class="form-control" name="birth_place" id="birth_place">
                     </div>
 
                     <div class="mb-3">
-                    <label for="gender" class="form-label">Sesso</label>
-                            <select name="gender">
-                            <option value="">Seleziona</option>
-                            <option value="f">F</option>
-                            <option value="m">M</option>
-                        </select>
+
+                        <span>Genere</span>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gender" id="gender_M">
+                            <label class="form-check-label" for="gender_M">
+                                Maschile
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gender" id="gender_F" checked>
+                            <label class="form-check-label" for="gender_F">
+                                Femminile
+                            </label>
+                        </div>
                     </div>
 
                     <div class="mb-3">
-                        <label for="nome_utente" class="form-label">Nome utente</label>
-                        <input type="text" class="form-control" name="nome_utente" id="nome utente">
+                        <label for="username" class="form-label">Nome utente</label>
+                        <input type="text" class="form-control" name="username" id="username">
                     </div>
 
                     <div class="mb-3">
-                        <label for="password" class="form-label">password</label>
+                        <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" name="password" id="password">
                     </div>
 
-                    <button class="btn btn-primary btn-sm" type="submit"> Accedi </button>
+                    <button class="btn btn-primary btn-sm" type="submit"> Crea </button>
 
                 </form>
 
             </div>
 
+            <div class="col-sm-4">
+      </div>
 
 
         </section>
     </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
