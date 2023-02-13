4f13e99ad28c2d011f1f2e4e51c498fc4d582eed
Emanuela <emanuela.velardo1@gmail.com>
2023-02-13 14:15:17 +0100

    ...

--- form_in_php/form.php
+++ form_in_php/form.php
@@ -1,13 +1,14 @@
 <!DOCTYPE HTML>
-<html>  
-<body>
-<form action="log-in-response.php" method="post">
-Name: <input type="text" value="Mario" name="first-name"><br>
-Cognome <input type="text" value="Rossi" name="last-name"><br>
-E-mail: <input type="email" value = "aaa@aaa.aaa" name="email"><br>
-password <input type="text" value = "segretissimo" name="password"><br>
-<input type="submit">
-</form>
+<html>
+    <body>
 
-</body>
+        <form action="log-in-response.php" method="POST">
+            Name: <input type="text" value="Mario" name="first-name"><br>
+            Cognome: <input type="text" value="Rossi" name="last-name"><br>
+            E-mail: <input type="email" value="aaa@aaa.aaa" name="email"><br>
+            Password: <input type="text" value="segretissimo" name="password"><br>
+            <input type="submit">
+        </form>
+
+    </body>
 </html>
---
+++ form_in_php/test/test_validazione_email.php
@@ -0,0 +1,39 @@
+<?php
+
+
+// elenco di situazioni in cui considero un email sbagliata 
+$emails = [
+    'a', ' ', 'a@', 'mario', '<h1>ciccio</h1>', 'a<@.it'
+];
+
+
+//require "";
+$file = scandir("."); 
+print_r($file);
+//require "./form_in_php/class/validator/ValidateMail.php";
+
+require "./form_in_php/class/validator/ValidateMail.php";
+
+
+$v = new ValidateMail();
+
+//in java per chiamare il metodo
+    // v.isValid('a');
+// in php uso 
+    // $v -> isValid($email);
+
+
+foreach ($emails as $index => $email) {
+
+// isValid restituisce un booleano
+   if( $v -> isValid($email) == false) {
+         echo "test superato per $email\n";
+    } else {
+        echo "test numero $index non superato per [$email]\n";
+    };
+    // $v -> getMessage();
+
+}
+
+
+?>
