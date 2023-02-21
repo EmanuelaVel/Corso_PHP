8a54a40dd0e75ec9b0f72ab400daeddaac2eaaaa
Emanuela <emanuela.velardo1@gmail.com>
2023-02-21 12:43:19 +0100

    ...

--- form_in_php/log-in.php
+++ form_in_php/log-in.php
@@ -1,27 +1,22 @@
 <!doctype html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
-
 <body>
   <header class="bg-light p-1">
     <h1 class="display-6">Applicazione demo</h1>
   </header>
-
   <main class="container">
 
     <section class="row">
-
       <div class="col-sm-4">
       </div>
 
       <div class="col-sm-4">
-
         <form class="mt-1 mt-md-5" action="log-in-response.php" method="POST">
 
           <div class="mb-3">
@@ -37,9 +32,7 @@
           <button class="btn btn-primary btn-sm" type="submit">Accedi</button>
         
         </form>
-
       </div>
-
       <div class="col-sm-4">
       </div>
     </section>
