cd8b3f7c7655b7ef42ca61f271742001b77f10f4
Emanuela <emanuela.velardo1@gmail.com>
2023-02-22 16:26:47 +0100

    ...

--- form_in_php/esercitazione_form/create-user.php
+++ form_in_php/esercitazione_form/create-user.php
@@ -51,28 +51,12 @@ $isValidatedNameClass = $validatorName->isValid($_POST['first_name']) ? '' : ' i
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Registrazione</title>
+    <title>Form di registrazione PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
-    <!-- Form di registrazione PHP
-
-        usare bootstrap
-        usare filter_input per ottenere le informazioni
-
-        create-user.php
-            - nome first_name 
-            - cognome last_name
-            - data di nascita birthday
-            - luogo di nascita birth_place
-            - sesso(M/F) gender
-            
-            - nome utente username
-            - password  password
-
-        Pagina che riceve i dati
-            register-user.php
+ 
 
 
 //se metto la classe is-invalid il messaggio di errore verrà visualizzato e comparirà il bordo rosso 
