2a93db818f79dc2806b19a82ced12fe0754f9cc5
Emanuela <emanuela.velardo1@gmail.com>
2023-03-09 21:30:10 +0100

    estrazione province

--- basi_json/estrazione_province.php
+++ basi_json/estrazione_province.php
@@ -6,56 +6,33 @@ $province_string = file_get_contents('province.json');
 //var_dump($province_string)
 
 
-// json_decode() : DECODIFICARE una stringa JSON in un oggetto/array PHP
-// in questo caso diventa un ARRAY di oggetti 
+// json_decode() :diventa un oggetto
 $province_object = json_decode($province_string); 
 //var_dump($province_object);
 
 
-//map() funzione degli array che serve a trasformare un array in un altro
-//$provincia = oggetto dell'array
-//array_map(funtion($elemento_array){}, array da cambiare)
- $province_array = array_map(function($provincia) {
-    return 
-    [
-        'nome' => $provincia -> nome,
-        'sigla' => $provincia -> sigla,
-        'regione' => $provincia -> regione
-       // 'regione_id' => $provincia -> regione
-
-    ];
-
- }, $province_object);
-
- var_dump($province_array);
-//cerca nella tabella regioni quella che sia chiam a"" e predni id
-
-
 $dsn = "mysql: host.=".DB_HOST.";dbname=".DB_NAME;
 
-//inseriamo le province nella tabella
 try {
-    //connection between PHP and a database server
+
     $conn = new PDO($dsn,DB_USER,DB_PASSWORD);
 
-// TRUNCATE TABLE : elimina i dati all'interno di una tabella, ma non la tabella stessa
     $conn -> query('TRUNCATE TABLE province');
 
-    //per ogni elemento $provincia dell array $province_array
-    foreach ($province_array as $citta => $provincia) {
+    foreach ($province_object as $provincia) {
 
-    $nome_provincia =addslashes($provincia ['nome']);
-    $sigla_provincia =addslashes($provincia ['sigla']);
-    $nome_regione  =addslashes($provincia ['regione']); 
+    $nome_provincia =addslashes($provincia->nome);
+    $sigla_provincia =addslashes($provincia ->sigla);
+    $regione_id = $provincia->regione;
 
-    $query=  "SELECT regione_id FROM regioni WHERE nome = '$nome_regione';";
+    $regione_id = $conn->query ("SELECT regione_id FROM regioni WHERE nome =\"$regione_id\"")->fetchColumn();
 
-    $id_regione = $conn ->query($query)->fetchColumn(); 
-  
+    print_r($regione_id);
 
-    $sql = "INSERT INTO province(nome, sigla, regione_id) VALUES('$nome_provincia', '$sigla_provincia', '$id_regione');";
-   echo $sql ."\n";
-    $conn -> query($sql);
+    $sql = "INSERT INTO province(nome, sigla, regione_id) VALUES('$nome_provincia', '$sigla_provincia', '$regione_id');";
+    echo $sql ."\n";
+     $conn -> query($sql);
+    
     }
 }catch(\Throwable $th) {
     throw $th;
--- basi_json/install.sql
+++ basi_json/install.sql
@@ -1,6 +1,6 @@
--- SQLBook: Code
--- 
 use form_in_php;
+
+	DROP TABLE regioni;
 
 CREATE TABLE regioni (
 	regione_id int NOT NULL AUTO_INCREMENT,
@@ -8,9 +8,20 @@ CREATE TABLE regioni (
 	PRIMARY KEY (regione_id)
 	) ;
 	
+	DROP TABLE province;
+
+
+	CREATE TABLE province (
+	provincia_id INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR (255) NOT NULL,
+	sigla VARCHAR (2) NOT NULL,
+	regione_id int NOT NULL,
+	PRIMARY KEY (provincia_id),
+	FOREIGN KEY (regione_id) REFERENCES regioni(regione_id)
+
+	);
+
 INSERT INTO regioni (nome) VALUE ('Abruzzo');
-	
-SELECT * FROM regioni;
 
 INSERT INTO regioni(nome) VALUES('Valle d\'Aosta/Vallée d\'Aoste');
 
